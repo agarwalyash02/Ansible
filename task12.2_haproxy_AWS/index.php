14bff94ef55cf611e4160846571d69956305e911
Yash Agarwal <49517141+agarwalyash02@users.noreply.github.com>
2021-01-11 01:26:49 +0530

    task 12.2 completed

---
+++ task12.2_haproxy_AWS/index.php
@@ -0,0 +1,15 @@
+</html>
+<body bgcolor="aqua">
+
+<pre>
+<?php
+
+print("refresh to connect with another server");
+print("\n\n");
+print `/usr/sbin/ifconfig eth0`;
+
+?>
+</pre>
+
+</body>
+</html>
